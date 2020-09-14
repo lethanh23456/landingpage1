403b2387db9ac83e527fdfc613b37b0aff46d88e
Bui Tuan <buituan7101999@gmail.com>
2020-09-14 15:50:31 +0700

    ecosystem

---
+++ ecosystem.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [{
+        name: "daotao-ptit-landingpage",
+        script: "npm run start",
+        env: {
+            NODE_ENV: "development",
+        },
+        env_production: {
+            NODE_ENV: "production",
+        }
+    }]
+}
