dca578aa5d544e2749c9a4988f4448ec3f82a3a0
nguyenhuyen06 <huyensu266@gmail.com>
2020-12-15 15:49:08 +0700

    doi ip

--- data/ip.js
+++ data/ip.js
@@ -1,8 +1,8 @@
 // export const ip = "https://apituyensinhptit.aisenote.com";
 // export const ip3 = "https://apituyensinhptit.aisenote.com";
 
-export const ip3 = 'https://xettuyenptitapi.aisenote.com';
-export const ip = 'https://xettuyenptitapi.aisenote.com';
+export const ip3 = "https://apiquanlydaotao.ptit.edu.vn/";
+export const ip = "https://apiquanlydaotao.ptit.edu.vn/";
 
 // export const ip3 = "http://203.162.10.108:3027";
 // export const ip = "http://203.162.10.108:3027";
