166bb4545b7120a7ab962acbcfa42870c9e3dff1
Dung Nguyen <dunglr@gmail.com>
2020-12-29 17:00:38 +0700

    sua file ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
+
 module.exports = {
     apps: [{
         name: "daotao-ptit-landingpage",
-        script: "npm run start",
+        script: "npm run start -- -p 3001",
         env: {
             NODE_ENV: "development",
         },
