5707dc8b5740d8e05a4a18a909a7de01c2f1b1df
vuthanhxuan99 <vuthanhxuan99@gmail.com>
2021-01-08 17:47:33 +0700

    add cursor nganh hoc

--- components/CardThongTinNganhHoc/CardCauTruc.style.js
+++ components/CardThongTinNganhHoc/CardCauTruc.style.js
@@ -1,6 +1,6 @@
 /* eslint-disable arrow-parens */
-import Container from 'components/UI/Container';
-import styled, { css } from 'styled-components';
+import Container from "components/UI/Container";
+import styled, { css } from "styled-components";
 
 // eslint-disable-next-line import/prefer-default-export
 export const RowHocKy = styled.div`
@@ -57,42 +57,43 @@ export const CardMon = styled.div`
   align-items: center;
 
   font-size: 14px;
+  cursor: pointer;
   line-height: 18px; /* or 133% */
   letter-spacing: 0.02em;
-  ${props => {
+  ${(props) => {
     let ret;
     switch (props.type) {
-      case 'Bắt buộc chung':
+      case "Bắt buộc chung":
         ret = css`
           background: #b1e0fc;
           color: black;
         `;
         break;
-      case 'Bắt buộc chung nhóm ngành':
+      case "Bắt buộc chung nhóm ngành":
         ret = css`
           background: #dad2f9;
           color: black;
         `;
         break;
-      case 'Bổ trợ ngành':
+      case "Bổ trợ ngành":
         ret = css`
           background: #ffeee6;
           color: black;
         `;
         break;
-      case 'Cơ sở ngành':
+      case "Cơ sở ngành":
         ret = css`
           background: #facccc;
           color: black;
         `;
         break;
-      case 'Chuyên ngành':
+      case "Chuyên ngành":
         ret = css`
           background: #f2c946;
           color: black;
         `;
         break;
-      case 'Thực tập':
+      case "Thực tập":
         ret = css`
           background: #1f61d2;
           color: white;
