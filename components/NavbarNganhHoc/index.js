7c138be38e4e5985be8bb36b6a8ec04192b1cbdb
vuthanhxuan99 <vuthanhxuan99@gmail.com>
2021-02-11 17:14:17 +0700

    last commit, happy new year

--- components/NavbarNganhHoc/index.js
+++ components/NavbarNganhHoc/index.js
@@ -1,19 +1,19 @@
 //
-import { Menu } from 'antd';
-import Box from 'components/Box';
-import Container from 'components/UI/Container';
-import Link from 'next/link';
-import React from 'react';
-import Sticky from 'react-stickynode';
-import { MenuDiv, MenuItem, Style } from './navbarNganhHoc.style';
+import { Menu } from "antd";
+import Box from "components/Box";
+import Container from "components/UI/Container";
+import Link from "next/link";
+import React from "react";
+import Sticky from "react-stickynode";
+import { MenuDiv, MenuItem, Style } from "./navbarNganhHoc.style";
 
 class Navbar extends React.Component {
   state = {
-    current: 'mail',
+    current: "mail",
   };
 
-  handleClick = e => {
-    console.log('click ', e);
+  handleClick = (e) => {
+    console.log("click ", e);
     this.setState({
       current: e.key,
     });
@@ -24,45 +24,62 @@ class Navbar extends React.Component {
     const { SubMenu } = Menu;
     return (
       <Box>
-        <Sticky top={95} bottomBoundary="#content" innerZ={999}>
+        <Sticky top={190} bottomBoundary="#content" innerZ={99999}>
           <Style>
             <Container>
               <MenuDiv>
                 <Menu
-                  style={{ backgroundColor: 'red', color: 'white' }}
+                  style={{ backgroundColor: "red", color: "white" }}
                   onClick={this.handleClick}
                   selectedKeys={[this.state.current]}
                   mode="horizontal"
-                // style={{ fontSize: 'calc(0.6em + 0.4vw)', padding: '15px 0' }}
+                  // style={{ fontSize: 'calc(0.6em + 0.4vw)', padding: '15px 0' }}
                 >
                   {this.props?.data?.map(({ name, url }) => {
-                    if (name === 'Cấu trúc chương trình') {
+                    if (name === "Cấu trúc chương trình") {
                       return (
-                        <SubMenu title={<span  style={{ color: 'white' }}>Cấu trúc chương trình</span>} style={{ backgroundColor: 'red' }}>
-                          {this.props?.monTinChi.map(({ tenChuyenNganh, anchor }) => (
-                            <Menu.Item key={tenChuyenNganh} style={{ fontSize: 14, padding: 8 }}>
-                              <Link href={`/nganhhoc/[idMaNganh]#${anchor}`} as={`/nganhhoc/${maNganh}#${anchor}`}>
-                              <a  > 
-                                  {/* <Icon type="file-protect" /> */}
-                                  {tenChuyenNganh}
-                                </a>
-                              </Link>
-                            </Menu.Item>
-                          ))}
+                        <SubMenu
+                          title={
+                            <span style={{ color: "white" }}>
+                              Cấu trúc chương trình
+                            </span>
+                          }
+                          style={{ backgroundColor: "red" }}
+                        >
+                          {this.props?.monTinChi.map(
+                            ({ tenChuyenNganh, anchor }) => (
+                              <Menu.Item
+                                key={tenChuyenNganh}
+                                style={{ fontSize: 14, padding: 8 }}
+                              >
+                                <Link
+                                  href={`/nganhhoc/[idMaNganh]#${anchor}`}
+                                  as={`/nganhhoc/${maNganh}#${anchor}`}
+                                >
+                                  <a>
+                                    {/* <Icon type="file-protect" /> */}
+                                    {tenChuyenNganh}
+                                  </a>
+                                </Link>
+                              </Menu.Item>
+                            )
+                          )}
                         </SubMenu>
-                      )
-                    }
-                    else {
+                      );
+                    } else {
                       return (
                         <MenuItem key={name}>
-                          <Link href={`/nganhhoc/[idMaNganh]${url}`} as={`/nganhhoc/${maNganh}${url}`}>
-                            <a   style={{ color: 'white' }}> 
+                          <Link
+                            href={`/nganhhoc/[idMaNganh]${url}`}
+                            as={`/nganhhoc/${maNganh}${url}`}
+                          >
+                            <a style={{ color: "white" }}>
                               {/* <Icon type="file-protect" /> */}
                               {name}
                             </a>
                           </Link>
                         </MenuItem>
-                      )
+                      );
                     }
                   })}
                 </Menu>
