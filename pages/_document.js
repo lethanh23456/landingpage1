5e8e29078a569fc14fdf3deef1e17baad5598850
Dung Nguyen <dunglr@gmail.com>
2021-02-19 20:09:26 +0700

    update fanpage messenger

--- pages/_document.js
+++ pages/_document.js
@@ -56,29 +56,33 @@ export default class CustomDocument extends Document {
     // logged_in_greeting="Xin chào, bạn muốn chúng tôi tư vấn gì không ?"
     // logged_out_greeting="Xin chào, bạn muốn chúng tôi tư vấn gì không ?">
     //   </div>`;
-    const fbCustomerChat = `<!-- Load Facebook SDK for JavaScript -->
-  <div id="fb-root"></div>
-  <script>
-    window.fbAsyncInit = function() {
-      FB.init({
-        xfbml            : true,
-        version          : 'v9.0'
-      });
-    };
+    const fbCustomerChat = `
+<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
 
-    (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
 
-  <!-- Your Chat Plugin code -->
-  <div class="fb-customerchat"
-    attribution=setup_tool
-    page_id="101402285211797">
-  </div>`;
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="245253749268217"
+  logged_in_greeting="Xin chào! Chúng tôi có thể giúp gì cho bạn?"
+  logged_out_greeting="Xin chào! Chúng tôi có thể giúp gì cho bạn?">
+      </div> 
+`;
     return (
       <html lang="vi">
         <Head>
