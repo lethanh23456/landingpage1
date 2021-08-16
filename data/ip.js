79d77a21fcf6815cbd7e140dbc18e43f31f6d331
Thanh Xuan Vu <vuthanhxuan99@gmail.com>
2021-08-16 20:39:48 +0700

    push sever

--- data/ip.js
+++ data/ip.js
@@ -1,11 +1,11 @@
 // export const ip3 = "https://apiquanlydaotao.ptit.edu.vn";
 // export const ip = "https://apiquanlydaotao.ptit.edu.vn";
 
-export const ip3 = "https://apituyensinhptit.aisenote.com";
-export const ip = "https://apituyensinhptit.aisenote.com"; //ip dev
+// export const ip3 = "https://apituyensinhptit.aisenote.com";
+// export const ip = "https://apituyensinhptit.aisenote.com"; //ip dev
 
-// export const ip3 = "https://apiquanlydaotao.ptit.edu.vn/";
-// export const ip = "https://apiquanlydaotao.ptit.edu.vn/";
+export const ip3 = "https://apiquanlydaotao.ptit.edu.vn/";
+export const ip = "https://apiquanlydaotao.ptit.edu.vn/";
 
 // export const ip3 = "http://203.162.10.108:3027";
 // export const ip = "http://203.162.10.108:3027";
