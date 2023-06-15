454de6516a8f84f0aea7234c3c24415fffc9cf05
gipsy17 <phanquangthanh0217@gmail.com>
2023-06-15 20:18:46 +0700

    fix bug

--- components/Table/TableXetTuyen.js
+++ components/Table/TableXetTuyen.js
@@ -67,7 +67,7 @@ export default function VanBangTable(props) {
       render:(val)=>{
         const color=val==='Trúng tuyển'?'green':'geekblue';
         return(
-            <div style={{color: color}}>Đủ điều kiện {val} (Trừ điều kiện tốt nghiệp THPT)</div>
+            <div style={{color: color}}>{val}</div>
         )
       }
     },
