d57b890f90c77e4b055b368e6c875a64d7e5f4a7
Phan Thành <quangthanh.phan@aisoft.com.vn>
2024-06-05 18:08:06 +0700

    fix bug

--- pages/tracuutuyensinh.js
+++ pages/tracuutuyensinh.js
@@ -97,9 +97,7 @@ const VBChungChi = (props) => {
       const hoTenNew = hoTen.trim();
       path = `hoTen=${hoTenNew}&ngaySinh=${ngaySinhNew}`;
     }
-    const data = await axios.get(
-      `${ip2}ket-qua-xet-tuyen-public/${new Date().getFullYear()}?${path}`
-    );
+    const data = await axios.get(`${ip2}ket-qua-xet-tuyen-public/2023?${path}`);
     // console.log(data.data.data, 'tra cuu vb')
     const arr = data?.data?.data ?? [];
     if (arr.length === 0) {
