cc68c2dc26886cd1173fb94aac63ec864abce051
lethanh23456 <thanhld2006.ptit@gmail.com>
2026-04-06 18:37:35 +0700

    upgrade: Next.js 12 for Netlify SSR support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules');
-const withFonts = require('next-fonts');
 const lessToJS = require('less-vars-to-js');
 const fs = require('fs');
 const path = require('path');
@@ -9,40 +6,34 @@ const themeVariables = lessToJS(
   fs.readFileSync(path.resolve('./assets/antd-custom.less'), 'utf8')
 );
 
-module.exports = withPlugins(
-  [
-    [withTM, { transpileModules: ['reusecore', 'common'] }],
-    withFonts,
-  ],
-  {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-        };
-      }
+module.exports = {
+  distDir: './.next',
+  trailingSlash: true,
+  transpilePackages: ['reusecore', 'common'],
 
-      // Handle Less/Antd
-      config.module.rules.push({
-        test: /\.less$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'less-loader',
-            options: {
-              lessOptions: {
-                javascriptEnabled: true,
-                modifyVars: themeVariables,
-              },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = { fs: false };
+    }
+
+    // Handle Less files (for antd)
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+              modifyVars: themeVariables,
             },
           },
-        ],
-      });
+        },
+      ],
+    });
 
-      return config;
-    },
-    distDir: './.next',
-    trailingSlash: true,
-  }
-);
+    return config;
+  },
+};
