92e680bbf14c1800e5d7eb03d925d7e7f7287010
lethanh23456 <thanhld2006.ptit@gmail.com>
2026-04-07 12:08:26 +0700

    fix

--- pages/vanbangchungchi/[idChiTiet].js
+++ pages/vanbangchungchi/[idChiTiet].js
@@ -6,6 +6,7 @@ import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import "./style.less";
+import { fontSize } from "styled-system";
 
 const PDFViewerV2 = dynamic(
   () => import("../../components/PDFViewerV2/index.js"),
@@ -62,8 +63,26 @@ const ChiTietVanBang = () => {
             paddingBottom: 30,
           }}
         >
-          <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 36, flexWrap: "wrap", gap: 16 }}>
-            <div style={{ fontSize: 28, fontWeight: "bold", color: "#1a253f", display: "flex", alignItems: "center", gap: 12 }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: 36,
+              flexWrap: "wrap",
+              gap: 16,
+            }}
+          >
+            <div
+              style={{
+                fontSize: 28,
+                fontWeight: "bold",
+                color: "#1a253f",
+                display: "flex",
+                alignItems: "center",
+                gap: 12,
+              }}
+            >
               <span>Chi tiết kết quả tra cứu</span>
 
               <svg
@@ -94,11 +113,22 @@ const ChiTietVanBang = () => {
 
             {record?._id && (
               <div className="vbcc-verified" style={{ marginBottom: 0 }}>
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M16 2C13.2311 2 10.5243 2.82109 8.22202 4.35943C5.91973 5.89777 4.12532 8.08427 3.06569 10.6424C2.00607 13.2006 1.72882 16.0155 2.26901 18.7313C2.80921 21.447 4.14258 23.9416 6.10051 25.8995C8.05845 27.8574 10.553 29.1908 13.2687 29.731C15.9845 30.2712 18.7994 29.9939 21.3576 28.9343C23.9157 27.8747 26.1022 26.0803 27.6406 23.778C29.1789 21.4757 30 18.7689 30 16C30 12.287 28.525 8.72602 25.8995 6.10051C23.274 3.475 19.713 2 16 2ZM14 21.5908L9.00001 16.5908L10.5906 15L14 18.4092L21.41 11L23.0057 12.5859L14 21.5908Z" fill="#24A148" />
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M16 2C13.2311 2 10.5243 2.82109 8.22202 4.35943C5.91973 5.89777 4.12532 8.08427 3.06569 10.6424C2.00607 13.2006 1.72882 16.0155 2.26901 18.7313C2.80921 21.447 4.14258 23.9416 6.10051 25.8995C8.05845 27.8574 10.553 29.1908 13.2687 29.731C15.9845 30.2712 18.7994 29.9939 21.3576 28.9343C23.9157 27.8747 26.1022 26.0803 27.6406 23.778C29.1789 21.4757 30 18.7689 30 16C30 12.287 28.525 8.72602 25.8995 6.10051C23.274 3.475 19.713 2 16 2ZM14 21.5908L9.00001 16.5908L10.5906 15L14 18.4092L21.41 11L23.0057 12.5859L14 21.5908Z"
+                    fill="#24A148"
+                  />
                 </svg>
 
-                <span>Thông tin đã được xác thực</span>
+                <span style={{ fontSize: "20px" }}>
+                  Thông tin đã được xác thực
+                </span>
               </div>
             )}
           </div>
@@ -107,7 +137,7 @@ const ChiTietVanBang = () => {
             <Row gutter={[12, 12]}>
               <Col span={24}>
                 <div className="vbcc-info-card">
-                  <div className="vbcc-info-title">
+                  <div className="vbcc-info-title" style={{ fontSize: "20px" }}>
                     Thông tin văn bằng
                   </div>
                   <Descriptions
@@ -157,7 +187,7 @@ const ChiTietVanBang = () => {
 
               <Col span={24}>
                 <div className="vbcc-info-card">
-                  <div className="vbcc-info-title">
+                  <div className="vbcc-info-title" style={{ fontSize: "20px" }}>
                     Thông tin quyết định
                   </div>
                   <Descriptions
@@ -171,7 +201,9 @@ const ChiTietVanBang = () => {
                     </Descriptions.Item>
                     <Descriptions.Item label="Ngày ban hành">
                       {record?.quyetDinh?.ngayBanHanh
-                        ? moment(record.quyetDinh.ngayBanHanh).format("DD/MM/YYYY")
+                        ? moment(record.quyetDinh.ngayBanHanh).format(
+                            "DD/MM/YYYY",
+                          )
                         : "--"}
                     </Descriptions.Item>
                     <Descriptions.Item label="Nội dung trích yếu" span={2}>
@@ -195,16 +227,17 @@ const ChiTietVanBang = () => {
               </Col>
 
               {(() => {
-                const templateElements = record?.quyetDinh?.bieuMau?.elements ?? [];
+                const templateElements =
+                  record?.quyetDinh?.bieuMau?.elements ?? [];
                 const dataElements = record?.templateData ?? [];
 
                 const elements = templateElements.length
                   ? templateElements.map((e) => ({
-                    ...e,
-                    value: dataElements.find(
-                      (d) => d.headerName === e.headerName,
-                    )?.value,
-                  }))
+                      ...e,
+                      value: dataElements.find(
+                        (d) => d.headerName === e.headerName,
+                      )?.value,
+                    }))
                   : dataElements;
 
                 const valuedElements = elements
@@ -216,7 +249,10 @@ const ChiTietVanBang = () => {
                     {!!valuedElements.length && (
                       <Col span={24}>
                         <div className="vbcc-info-card">
-                          <div className="vbcc-info-title">
+                          <div
+                            className="vbcc-info-title"
+                            style={{ fontSize: "20px" }}
+                          >
                             Thông tin phụ lục
                           </div>
                           <Descriptions
@@ -251,7 +287,10 @@ const ChiTietVanBang = () => {
                               i.type === "Text" ? <span>{val}</span> : val,
                           })) ?? [];
 
-                        if (Array.isArray(item.value) && item.value.length > 0) {
+                        if (
+                          Array.isArray(item.value) &&
+                          item.value.length > 0
+                        ) {
                           return (
                             <Col span={24} key={index}>
                               <div className="vbcc-info-card">
